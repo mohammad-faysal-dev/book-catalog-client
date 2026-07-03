5dfc80f54d8c051ec5cebf625c1194f4a4f84af1
Faysal2123 <foysal2005n1@gmail.com>
2026-07-04 01:09:11 +0600

    feat: add registerUser mutation endpoint

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -3,8 +3,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/auth",
+    baseUrl: "http://localhost:5000/api/v1",
   }),
-  tagTypes: ["Auth"],
-  endpoints: (builder)=>({})
+  tagTypes: ["User"],
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (data) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    
+  }),
 });
