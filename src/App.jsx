9f19a471c05c2abb3d63c3d74e7499a531bf92a1
Faysal2123 <foysal2005n1@gmail.com>
2026-07-06 00:39:10 +0600

    feat: set up app routing with layout and toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Layout from './components/layout/Layout';
+import Home from './pages/Home';
 
-function App() {
-
+const App = () => {
   return (
-    <div className='text-6xl'>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: '10px',
+            background: '#1f2937',
+            color: '#fff',
+            fontSize: '14px',
+          },
+          success: {
+            iconTheme: { primary: '#4C6EF5', secondary: '#fff' },
+          },
+          error: {
+            iconTheme: { primary: '#ef4444', secondary: '#fff' },
+          },
+        }}
+      />
 
-export default App
+      <Routes>
+        <Route element={<Layout />}>
+          {/* Public routes */}
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
